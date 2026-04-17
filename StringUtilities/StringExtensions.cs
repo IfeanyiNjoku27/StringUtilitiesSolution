65d8755e4a6b8c1ce8e57110d15216513d915cb1
Ifeanyi Njoku <injoku@my.centennialcollege.ca>
2026-04-16 20:34:41 -0400

    Initial commit: Add StringExtensions

---
+++ StringUtilities/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace StringUtilities;
+
+public static class StringExtensions
+{
+    public static bool StartsWithUpperCase(this string str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+        {
+            return false;
+        }
+        return char.IsUpper(str[0]);
+    }
+}
